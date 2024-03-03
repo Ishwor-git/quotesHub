01c6b2d9fc3bde96e663629078d7360d436cedf9
Ishwor-git <raishwor2@gmail.com>
2024-03-03 23:29:22 +0545

    dynamic theme added

--- src/components/quote.jsx
+++ src/components/quote.jsx
@@ -1,109 +1,20 @@
 // import bgImg from "../assets/motivation1.jpg";
+import theme from "../data/theme";
 const Quote = ({
   quote,
   author,
-  catag = "unknown",
+  catag,
   handleClick = () => {
     console.log("action not defined for this component");
   },
 }) => {
   // const bgImg = require(`./../assets/${catag}.jpg`);
+  console.log(catag);
   const bgImg = require(`./../assets/${catag}.jpg`);
 
-  const theme = {
-    success: {
-      bgHover: "hover:bg-green-600",
-      text: "text-green-600",
-      gradient: {
-        from: "from-green-200",
-        to: "to-green-600",
-      },
-      border: "border-green-600",
-    },
-    business: {
-      bgHover: "hover:bg-blue-600",
-      text: "text-blue-600",
-      gradient: {
-        from: "from-blue-200",
-        to: "to-blue-600",
-      },
-      border: "border-blue-600",
-    },
-    competition: {
-      bgHover: "hover:bg-yellow-600",
-      text: "text-yellow-600",
-      gradient: {
-        from: "from-yellow-200",
-        to: "to-yellow-600",
-      },
-      border: "border-yellow-600",
-    },
-    life: {
-      bgHover: "hover:bg-red-600",
-      text: "text-red-600",
-      gradient: {
-        from: "from-red-200",
-        to: "to-red-600",
-      },
-      border: "border-red-600",
-    },
-    humorous: {
-      bgHover: "hover:bg-purple-600",
-      text: "text-purple-600",
-      gradient: {
-        from: "from-purple-200",
-        to: "to-purple-600",
-      },
-      border: "border-purple-600",
-    },
-    love: {
-      bgHover: "hover:bg-pink-600",
-      text: "text-pink-600",
-      gradient: {
-        from: "from-pink-200",
-        to: "to-pink-600",
-      },
-      border: "border-pink-600",
-    },
-    technology: {
-      bgHover: "hover:bg-indigo-600",
-      text: "text-indigo-600",
-      gradient: {
-        from: "from-indigo-200",
-        to: "to-indigo-600",
-      },
-      border: "border-indigo-600",
-    },
-    politics: {
-      bgHover: "hover:bg-gray-600",
-      text: "text-gray-600",
-      gradient: {
-        from: "from-gray-200",
-        to: "to-gray-600",
-      },
-      border: "border-gray-600",
-    },
-    education: {
-      bgHover: "hover:bg-blue-600",
-      text: "text-blue-600",
-      gradient: {
-        from: "from-blue-200",
-        to: "to-blue-600",
-      },
-      border: "border-blue-600",
-    },
-    unknown: {
-      bgHover: "hover:bg-gray-600",
-      text: "text-gray-600",
-      gradient: {
-        from: "from-gray-200",
-        to: "to-gray-600",
-      },
-      border: "border-gray-600",
-    },
-  };
-
-  const test = "education";
+  const test = catag.slice();
+  // console.log(theme[catag]);
+  // const test = "education";
 
   return (
     <>
@@ -113,14 +24,16 @@ const Quote = ({
           backgroundImage: `url(${bgImg})`,
         }}
       >
-        <div className=" w-full h-full flex flex-col justify-between items-center box-border  bg-black bg-opacity-50 rounded-xl">
+        <div
+          className={` w-full h-full flex flex-col justify-between items-center box-border  ${theme[catag].bg} bg-opacity-50 rounded-xl`}
+        >
           <div className="w-full h-full flex flex-col justify-center items-center">
             <h2 className="text-white font-medium text-xl p-4">{quote}</h2>
             <h1 className="text-white font-medium text-lg p-4">- {author}</h1>
           </div>
           <div className="flex flex-row justify-center w-full mb-10">
             <button
-              className={` text-purple-600 py-1 px-4 border border-purple-600 rounded-full bg-white ${theme.competition.bgHover} transition-all duration-300`}
+              className={` ${theme[catag].text} py-1 px-4 border ${theme[catag].border} rounded-full bg-white ${theme[catag].bgHover} hover:text-white transition-all duration-300`}
               onClick={handleClick}
             >
               get another quote
@@ -128,7 +41,7 @@ const Quote = ({
           </div>
         </div>
       </div>
-      <div className=" max-md:hidden w-8/12 h-80  flex flex-row justify-center items-center my-8">
+      <div className=" max-md:hidden w-8/12 h-96  flex flex-row justify-center items-center my-8">
         <div
           className=" h-full w-2/5 bg-cover bg-center bg-no-repeat rounded-l-3xl"
           style={{
@@ -136,12 +49,12 @@ const Quote = ({
           }}
         ></div>
         <div
-          className={` h-full w-3/5 flex flex-col justify-center items-center bg-gradient-to-r ${theme[test].gradient.from} to-purple-600 rounded-r-3xl`}
+          className={` h-full w-3/5 flex flex-col justify-center items-center bg-gradient-to-r ${theme[catag].gradient.from} ${theme[catag].gradient.to} rounded-r-3xl`}
         >
           <h2 className="text-white font-semibold text-2xl p-4">{quote}</h2>
           <h1 className="text-white font-semibold text-xl p-4">- {author}</h1>
           <button
-            className=" mt-4 mb-4 text-purple-600 py-2 px-8 border border-purple-600 rounded-xl bg-white hover:bg-purple-600 hover:text-white transition-all duration-300"
+            className={`mt-4 mb-4 ${theme[catag].text} py-2 px-8 border ${theme[catag].border} rounded-xl bg-white ${theme[test].bgHover} hover:text-white transition-all duration-300`}
             onClick={handleClick}
           >
             get another quote
---
+++ src/data/theme.js
@@ -0,0 +1,127 @@
+const theme = {
+  success: {
+    bg: "bg-green-600",
+    bgHover: "hover:bg-green-600",
+    text: "text-green-600",
+    gradient: {
+      from: "from-green-200",
+      to: "to-green-600",
+    },
+    border: "border-green-600",
+  },
+  business: {
+    bg: "bg-cyan-600",
+    bgHover: "hover:bg-cyan-600",
+    text: "text-cyan-600",
+    gradient: {
+      from: "from-cyan-200",
+      to: "to-cyan-600",
+    },
+    border: "border-cyan-600",
+  },
+  competition: {
+    bg: "bg-gray-600",
+    bgHover: "hover:bg-gray-600",
+    text: "text-gray-600",
+    gradient: {
+      from: "from-gray-200",
+      to: "to-gray-600",
+    },
+    border: "border-yellow-600",
+  },
+  life: {
+    bg: "bg-amber-600",
+    bgHover: "hover:bg-amber-600",
+    text: "text-amber-600",
+    gradient: {
+      from: "from-amber-200",
+      to: "to-amber-600",
+    },
+    border: "border-amber-600",
+  },
+  humorous: {
+    bg: "bg-purple-600",
+    bgHover: "hover:bg-purple-600",
+    text: "text-purple-600",
+    gradient: {
+      from: "from-purple-200",
+      to: "to-purple-600",
+    },
+    border: "border-purple-600",
+  },
+  love: {
+    bg: "bg-pink-600",
+    bgHover: "hover:bg-pink-600",
+    text: "text-pink-600",
+    gradient: {
+      from: "from-pink-200",
+      to: "to-pink-600",
+    },
+    border: "border-pink-600",
+  },
+  technology: {
+    bg: "bg-indigo-600",
+    bgHover: "hover:bg-indigo-600",
+    text: "text-indigo-600",
+    gradient: {
+      from: "from-indigo-200",
+      to: "to-indigo-600",
+    },
+    border: "border-indigo-600",
+  },
+  politics: {
+    bg: "bg-zinc-600",
+    bgHover: "hover:bg-zinc-600",
+    text: "text-zinc-600",
+    gradient: {
+      from: "from-zinc-200",
+      to: "to-zinc-600",
+    },
+    border: "border-zinc-600",
+  },
+  education: {
+    bg: "bg-blue-600",
+    bgHover: "hover:bg-blue-600",
+    text: "text-blue-600",
+    gradient: {
+      from: "from-blue-200",
+      to: "to-blue-600",
+    },
+    border: "border-blue-600",
+  },
+
+  sports: {
+    bg: "bg-lime-600",
+    bgHover: "hover:bg-lime-600",
+    text: "text-lime-600",
+    gradient: {
+      from: "from-lime-200",
+      to: "to-lime-600",
+    },
+    border: "border-lime-600",
+  },
+
+  history: {
+    bg: "bg-stone-600",
+    bgHover: "hover:bg-stone-600",
+    text: "text-stone-600",
+    gradient: {
+      from: "from-stone-200",
+      to: "to-stone-600",
+    },
+    border: "border-stone-600",
+  },
+
+  unknown: {
+    bg: "bg-gray-600",
+    bgHover: "hover:bg-gray-600",
+    text: "text-gray-600",
+    gradient: {
+      from: "from-gray-200",
+      to: "to-gray-600",
+    },
+    border: "border-gray-600",
+  },
+};
+
+export default theme;
